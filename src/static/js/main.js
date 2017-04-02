cb86ac1d57658d9233340ca02f70f477e6fa7368
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2017-04-02 00:49:05 -0500

    home

---
+++ src/static/js/main.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+
+	$(".header__icons__open").click(function(event) {
+		/* Act on the event */
+		event.preventDefault();
+		$(".header__menu").addClass('header__menu--responsive-active')
+		$(this).addClass('header__icons__none')
+		$(".header__icons__close").addClass('header__icons__active')
+		$(".header__menu .header__menu__icons__close").addClass('header__menu--responsive-active')
+	});
+
+	$(".header__icons__close").click(function(event) {
+		/* Act on the event */
+		event.preventDefault();
+		console.log("tst")
+
+		$(".header__menu").removeClass('header__menu--responsive-active')
+		$(".header__icons__open").removeClass('header__icons__none')
+		$(this).removeClass('header__icons__active')
+		$(".header__menu .header__menu__icons__close").removeClass('header__menu--responsive-active')
+	});
+});
