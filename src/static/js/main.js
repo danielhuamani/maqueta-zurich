4c691c5a57bc856d0e1bd3b5cbee63761189b25e
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2017-04-05 06:31:51 -0500

    servicios contacto

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -7,13 +7,14 @@ jQuery(document).ready(function($) {
 		$(this).addClass('header__icons__none')
 		$(".header__icons__close").addClass('header__icons__active')
 		$(".header__menu .header__menu__icons__close").addClass('header__menu--responsive-active')
+		$(".overlay").addClass("overlay--active")
 	});
 
 	$(".header__icons__close").click(function(event) {
 		/* Act on the event */
 		event.preventDefault();
 		console.log("tst")
-
+		$(".overlay").removeClass("overlay--active")
 		$(".header__menu").removeClass('header__menu--responsive-active')
 		$(".header__icons__open").removeClass('header__icons__none')
 		$(this).removeClass('header__icons__active')
