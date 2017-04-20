60e813467b6c6585d3dfb54f89bb27d17b536792
Yelson Chevarrias <chevarrias@gmail.com>
2017-04-20 14:31:02 -0500

    Reestructuración en el modo de trabajo con GULP
    Se agregó nuevas funciones para un uso efectivo de recursos y para un flujo de trabajo más dinámico y rapido.
    Se implementó un task y una función para tratar cada archivo .styl de manera independiente asi evitar recompilar archivos no modificados, de esta manera el tiempo de visualización sería mucho más rapido. actualmente se ha seteado ese tiempo en 369 microsegundos.
    
    Tambien se agregó un token para identificar el build tanto en producción como en local.
    
    Se eliminó los módulos que no se utilizaban y algunos fueron reutilizados.

--- gulpfile.js
+++ gulpfile.js
@@ -3,41 +3,56 @@ var stylus = require('gulp-stylus');
 var jade = require('gulp-jade');
 var swig = require('gulp-swig');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
 var connect = require('gulp-connect');
 var nib = require('nib');
-var historyApiFallback = require('connect-history-api-fallback');
 var directorio = {
-
   html_dist: 'src/build/',
+  css_dist: 'src/static/css/build/',
+  css_dist_files: 'src/static/css/build/**/*.css',
+  css: 'src/static/css/',
   html: 'src/templates/**/*.html',
   stylus: 'src/static/stylus/*.styl',
-  stylus_blocks: 'src/static/stylus/**/*.styl',
-
-
+  stylus_blocks: ['src/static/stylus/**/*.styl', '!src/static/stylus/base/variables.styl'],
 };
 
-gulp.task('stylus', function () {
+/*gulp.task('stylus', function () {
   gulp.src(directorio.stylus)
     .pipe(plumber())
     .pipe(stylus({
       use: nib(),
       compress: true
-
       }))
     .pipe(gulp.dest('src/static/css/'))
     .pipe(connect.reload());
 
-});
+});*/
+var glob_time_offset = 5128;
+
+function makeid(){
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < 8; i++ )
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+function StartsWith(s1, s2) {
+  return (s1.length >= s2.length && s1.substr(0, s2.length) == s2);
+}
+
 gulp.task('stylus_blocks', () => {
   return gulp.src(directorio.stylus_blocks)
     .pipe(plumber())
     .pipe(stylus({
       use: nib(),
-      compress: true
+      compress: false,
+      import: ['nib','../base/variables']
     }))
-    .pipe(gulp.dest('src/static/css/'))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(directorio.css_dist))
     .pipe(connect.reload())
 });
 
@@ -57,19 +72,17 @@ var opt = {
         cache: false
       },
       data: {
-        STATIC_URL : '/static/'
+        STATIC_URL : '/static/',
+        make_id : makeid()
       }
     }
 
 gulp.task('templates', function() {
-
   return gulp.src(directorio.html)
-    .pipe(plumber())
+    //.pipe(plumber())
     .pipe(swig(opt))
     .pipe(gulp.dest(directorio.html_dist))
     .pipe(connect.reload());
-
-
 });
 /*gulp.task('templates_blocks', function() {
 
@@ -83,27 +96,53 @@ gulp.task('templates', function() {
 
 
 });*/
-gulp.task('watch', function() {
 
-  gulp.watch('src/static/stylus/*.styl', ['stylus']),
-  gulp.watch('src/templates/**/*.html', ['templates'])
-  gulp.watch(directorio.stylus_blocks, ['stylus_blocks'])
-
+gulp.task('concat_css', function () {
+  setTimeout(function () {
+    return gulp.src(directorio.css_dist_files)
+    //.pipe(plumber())
+    .pipe(concat('main.css'))
+    //.pipe(plumber.stop())
+    .pipe(gulp.dest(directorio.css)).on('end', function(){
+        console.log('>>>>>>>>>> Empaquetado de css Completado ...');
+      })
+    .pipe(connect.reload());
+  }, glob_time_offset);
+  glob_time_offset = 369;
 });
 
-//creacioon  del server para el livereload
+function styl_com_con(file){
+  if (StartsWith(file.path,"/")){
+    file_name = file.path.split('/')[file.path.split('/').length - 1];
+  }else{
+    file_name = file.path.split('\\')[file.path.split('\\').length - 1];
+  }
+  console.log('>>>>>>>>>> Compiling '+file_name+' : running tasks...');
+  gulp.src(file.path)
+  .pipe(plumber())
+  .pipe(stylus({ use: nib(),  import: ['nib','../base/variables']}))
+  .pipe(plumber.stop())
+  .pipe(gulp.dest(file.path.replace('stylus','css/build')))
+  console.log('>>>>>>>>>> Compiled! '+file_name);
+}
 
+//creacioon  del server para el livereload
 gulp.task('connect', function() {
   connect.server({
     root: 'src/',
     hostname: '0.0.0.0',
     livereload: true,
     port: '3000',
-    open: true,
-    middleware: function(connect, opt) {
-      return [ historyApiFallback() ];
-    }
+    open: true
   });
 });
-gulp.task('default', ['stylus', 'templates', 'stylus_blocks','watch', 'connect']);
+
+gulp.task('watch', function() {
+  //gulp.watch('src/static/stylus/*.styl', ['stylus'])
+  gulp.watch(directorio.stylus_blocks, ['concat_css']).on('change', function(file) {return styl_com_con(file);});
+  gulp.watch('src/templates/**/*.html', ['templates']);
+  //gulp.watch(directorio.stylus_blocks, ['stylus_blocks']);
+});
+
+gulp.task('default', ['stylus_blocks','templates', 'connect','watch','concat_css']);
 //creacioon  del server para el livereload
